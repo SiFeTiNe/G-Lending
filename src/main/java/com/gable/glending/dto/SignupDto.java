593b056f32b05d3cc72a99b4bdd36a59677d4cc4
SiFeTiNe <keyboard2543@pop-os.localdomain>
2023-05-22 18:23:10 +0700

    Implements Input Validation

--- src/main/java/com/gable/glending/dto/SignupDto.java
+++ src/main/java/com/gable/glending/dto/SignupDto.java
@@ -1,11 +1,33 @@
 package com.gable.glending.dto;
 
+import com.gable.glending.validation.ValidPassword;
 import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 public class SignupDto {
+
+    @NotBlank
+    @Size(min=4, message = "Username must have at least 4 characters")
     private String username;
+
+    @NotBlank
+    @ValidPassword
     private String password;
+
+    @NotBlank(message = "First name is required")
+    @Pattern(regexp = "^[a-zA-Z]+$",
+            message = "First name can only contain letters")
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
+    private String email;
 }
